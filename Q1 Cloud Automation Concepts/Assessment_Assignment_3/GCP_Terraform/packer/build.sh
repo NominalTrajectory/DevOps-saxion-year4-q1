d373e5d27186b4bdc21ad1f9f6e6485a3a54986d
BFL-Psycho <xocpref@gmail.com>
2020-10-31 18:10:03 +0100

    Assignment 3 initiated

---
+++ Q1 Cloud Automation Concepts/Assessment_Assignment_3/GCP_Terraform/packer/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# To enable detailed log execute
+# export PACKER_LOG=1
+
+# Loading environment variables
+set -e
+
+source .envrc
+
+packer validate \
+    -var zone="$CLOUDSDK_COMPUTE_ZONE" \
+    -var project_id="$TF_ADMIN" \
+    -var service_account_json="$TF_CREDS" \
+    packer/k8s-image.json
+
+packer build \
+    -var zone="$CLOUDSDK_COMPUTE_ZONE" \
+    -var project_id="$TF_ADMIN" \
+    -var service_account_json="$TF_CREDS" \
+    packer/k8s-image.json
+
